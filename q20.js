d16a72266bcd7bcb7602222bb07691a7919f6833
IlayTissona <ilay211@gmail.com>
2021-01-19 19:47:18 +0200

    question 20 fixed
    added a loop to delete duplications in the output array, so that every item will only appear once even if he apperes more than twice in the original array.

--- q20.js
+++ q20.js
@@ -3,15 +3,25 @@ function run() {
 	const output = document.getElementById("output");
 	inParsed = JSON.parse(input);
 	let temp = 0;
-	let out = "Duplicates: ";
+	let out = [];
 	for (let i = 0; i < inParsed.length; i++) {
 		temp = inParsed[i];
 		for (let j = 0; j < inParsed.length; j++) {
 			if (inParsed[j] === temp && j !== i && inParsed[j] !== null) {
-				out += inParsed[j] + ", ";
+				out.push(inParsed[j]);
 				inParsed[j] = null;
 			}
 		}
 	}
-	output.innerText = out;
+	for (let i = 0; i < out.length; i++) {
+		temp = out[i];
+		for (let j = 0; j < out.length; j++) {
+			if (out[j] === temp && j !== i && out[j] !== null) {
+				out.splice(j, 1);
+				inParsed[j] = null;
+			}
+		}
+	}
+
+	output.innerText = "Duplicates: " + out;
 }

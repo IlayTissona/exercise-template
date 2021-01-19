ec44a109b2432634aedf40a1891375aa69f2e30b
IlayTissona <ilay211@gmail.com>
2021-01-19 14:19:23 +0200

    question 1

--- q1.js
+++ q1.js
@@ -1,11 +1,12 @@
 function run() {
-    const input = document.getElementById("input").value;
-    const output = document.getElementById("output");
-    let out = "";
-/**/
-    // write your code here
-    // out = input + 5;
+	const input = document.getElementById("input").value;
+	const output = document.getElementById("output");
+	let out = "";
 
-/**/
-    output.innerText = out;
+	if (input[0] === "[" && input[input.length - 1] === "]") {
+		out = true;
+	} else {
+		out = false;
+	}
+	output.innerText = out;
 }

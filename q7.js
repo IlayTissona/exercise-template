af2380772313839fa052633a559f51bafa626b52
IlayTissona <ilay211@gmail.com>
2021-01-19 15:07:51 +0200

    question 7
    some bugs fixed in functions.js

--- q7.js
+++ q7.js
@@ -1,8 +1,22 @@
 function run() {
 	const input = document.getElementById("input").value;
 	const output = document.getElementById("output");
-	let out = "";
-
+	let out = [];
+	let arr = textToArr(input);
+	for (let i = 0; i < arr.length; i++) {
+		arr[i] = Number(arr[i]);
+	}
+	let temp = 0;
+	for (let x = 0; x < arr.length; x++) {
+		for (let y = x; y < arr.length; y++) {
+			if (arr[x] > arr[y]) {
+				temp = arr[x];
+				arr[x] = arr[y];
+				arr[y] = temp;
+			}
+		}
+		out.push(arr[x]);
+	}
 	output.innerText = out;
 }
 

f9f724a4d54bc189a5f8439a1830c46310243053
IlayTissona <ilay211@gmail.com>
2021-01-20 12:21:28 +0200

    question 24 fixed

--- q24.js
+++ q24.js
@@ -4,15 +4,15 @@ function run() {
 	let inParsed = textToArr(input);
 	console.log(inParsed);
 	for (let i = 0; i < inParsed.length; i++) {
-		if (isNaN(inParsed[i]) || inParsed[i] === 0 || inParsed[i] === "") {
+		if (isNaN(inParsed[i]) || inParsed[i] == 0 || inParsed[i] === "") {
 			inParsed.splice(i, 1);
+			i--;
 			console.log(inParsed);
 		}
 	}
 
 	output.innerText = inParsed;
 }
-
 function textToArr(input) {
 	let out = [];
 	let index = 0;
